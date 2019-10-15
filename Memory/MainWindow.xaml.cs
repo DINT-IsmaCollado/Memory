8b40545083f236e1f57a11478cdfb427e27a4d07
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2019-10-15 12:58:20 +0200

    Introduce cartas

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -25,10 +25,93 @@ namespace Memory
         {
             InitializeComponent();
         }
-
-        private void Window_ContentRendered(object sender, EventArgs e)
+        private void ButtonIniciar_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (RadioButtonBaja.IsChecked == true)
+                FilasPartidaFacil();
+            if (RadioButtonMedia.IsChecked == true)
+                FilasPartidaMedia();
+            if (RadioButtonAlta.IsChecked == true)
+                FilasPartidaAlta();
+        }
+
+        private void FilasPartidaFacil()
+        {
+            GridCartas.Children.Clear();
+
+            Border borde;
+            TextBlock tb;
+            Viewbox vb;
+
+            const int NUM_FILAS = 3;
+            for (int indiceFilas = 0; indiceFilas <= NUM_FILAS; indiceFilas++)
+            {
+                    borde = new Border();
+                    tb = new TextBlock();
+                    vb = new Viewbox();
+                    GridCartas.RowDefinitions.Add(new RowDefinition());
+                    GridCartas.Children.Add(borde);
+                    borde.Child = vb;
+                    vb.Child = tb;
+    
+                    tb.FontFamily = new FontFamily("Webdings");
+    
+
+                    tb.Text += "s";
+            }
+        }
+
+        private void FilasPartidaMedia()
+        {
+
+            const int NUM_FILAS = 4;
+            for (int indiceFilas = 0; indiceFilas < NUM_FILAS; indiceFilas++)
+            {
+                GridCartas.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        private void FilasPartidaAlta()
+        {
+
+            GridCartas.Children.Clear();
+
+            const int NUM_FILAS = 5;
+            for (int indiceFilas = 0; indiceFilas < NUM_FILAS; indiceFilas++)
+            {
+                GridCartas.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        private void EscribirCartas()
+        {
+            Border borde;
+            TextBlock tb;
+            Viewbox vb;
+
+            for (int contadorFila = 0; contadorFila < GridCartas.RowDefinitions.Count; contadorFila++)
+            {
+                for (int contadorColumna = 0; contadorColumna < 4; contadorColumna++)
+                {
+                    borde = new Border();
+                    tb = new TextBlock();
+                    vb = new Viewbox();
+
+
+                    GridCartas.Children.Add(borde);
+                    borde.Child = vb;
+                    vb.Child = tb;
+
+                    tb.FontFamily = new FontFamily("Webdings");
+
+                    Grid.SetColumn(tb, contadorColumna);
+                    Grid.SetRow(tb, contadorFila);
+
+                    tb.Text = "s";
+
+                    borde.Margin = new Thickness(3);
+                }
+            }
         }
     }
 }
